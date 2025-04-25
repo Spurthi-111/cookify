c472da51df43c6e111f017d40f6aa98a93429007
Sai Spurthi Gudelli <saispurthigudelli2004email.com>
2025-04-25 22:59:35 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+export default function Footer(){
+    return (
+        <div className="footer container">
+            <div className="footer-section">
+                <p className="title">Cookify.com</p>
+                <p>Taste the world with Cookify — free recipes, no limits.</p>
+                <p>&copy; 2025 | All Rights Reserved</p>
+            </div>
+            <div className="footer-section">
+                <p className="title">Contact Us</p>
+                <p>Cookify44@gmail.com</p>
+                <p>+91-93241-94544</p>
+                <p>Hyderabad</p>
+            </div>
+            <div className="footer-section">
+                <p className="title">Socials</p>
+                <p>Facebook</p>
+                <p>Twitter</p>
+                <p>Instagram</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/HeroSection.js
@@ -0,0 +1,29 @@
+import CustomImage from "./CustomImage"
+
+export default function HeroSection(){
+    const images = [
+        "/img/gallery/img_1.jpg",
+        "/img/gallery/img_2.jpg",
+        "/img/gallery/img_3.jpg",
+        "/img/gallery/img_4.jpg",
+        "/img/gallery/img_5.jpg",
+        "/img/gallery/img_6.jpg",
+        "/img/gallery/img_7.jpg",
+        "/img/gallery/img_8.jpg",
+        "/img/gallery/img_9.jpg"
+    ]
+    return (
+        <div className="section hero">
+            <div className="col typography">
+                <h1 className="title">What Are We About</h1>
+                <p className="info">Cookify is your go-to destination to satisfy both your taste buds and soul with delicious recipes from every cuisine — and the best part, it's completely free!</p>
+                <button className="btn">explore now</button>
+            </div>
+            <div className="col gallery">
+                { images.map((src, index) => (
+                    <CustomImage key={index} imgSrc={src} pt={"90%"} />
+                )) }
+            </div>
+        </div>
+    )
+}
